500a06752c1e32999e66b51397d5b6665f113631
siyu.yusi <siyu.yusi@alibaba-inc.com>
2019-05-06 12:23:04 +0800

    Add netty client demo

--- src/main/java/com/github/bunkyoneko/examples/NettyEchoClient.java
+++ src/main/java/com/github/bunkyoneko/examples/NettyEchoClient.java
@@ -2,7 +2,7 @@ package com.github.bunkyoneko.examples;
 
 import java.net.InetSocketAddress;
 
-import io.netty.bootstrap.ServerBootstrap;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
@@ -11,35 +11,34 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
- * Netty echo server demo
+ * Netty client echo demo
  *
  * @author bunkyoneko
- * @date 2019-05-05
+ * @date 2019-05-06
  */
-public class NettyEchoServer {
+public class NettyEchoClient {
+    private final String host;
     private final int port;
 
-    public NettyEchoServer(int port) {
+    public NettyEchoClient(String host, int port) {
+        this.host = host;
         this.port = port;
     }
 
     public void start() throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
-
         try {
-            ServerBootstrap b = new ServerBootstrap();
+            Bootstrap b = new Bootstrap();
             b.group(group)
              .channel(NioServerSocketChannel.class)
-             .localAddress(new InetSocketAddress(port))
-             .childHandler(new ChannelInitializer<SocketChannel>() {
+             .remoteAddress(new InetSocketAddress(host, port))
+             .handler(new ChannelInitializer<SocketChannel>() {
                  @Override
                  public void initChannel(SocketChannel ch) throws Exception {
-                     ch.pipeline().addLast(new NettyEchoServerHandler());
+                     ch.pipeline().addLast(new NettyEchoClientHandler());
                  }
              });
-
-            ChannelFuture f = b.bind().sync();
-            System.out.println(NettyEchoServer.class.getName() + f.channel().localAddress());
+            ChannelFuture f = b.connect().sync();
             f.channel().closeFuture().sync();
         } finally {
             group.shutdownGracefully().sync();
